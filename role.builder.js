500bc283a3fa87d24197febb3de09995c5f8ea9d
Icelll <52091543+Icelll@users.noreply.github.com>
2019-08-21 17:57:57 +0800

    Update role.builder.js

--- role.builder.js
+++ role.builder.js
@@ -9,7 +9,15 @@
 
 var roleBuilder = {
     run: function(a_creep) {
-                
+        var constructionSite = a_creep.pos.findClosestByRange(FIND_MY_CONSTRUCTION_SITES);
+        if(constructionSite)
+        {
+        	if(a_creep.build(constructionSite)==ERR_NOT_IN_RANGE)
+        	{
+        		a_creep.moveTo(constructionSite);
+        	}
+        }
+
     }
 };
 
